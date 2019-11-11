8908b98334ea46ac9030bd1c28de6ce054f7dd7e
LiParanoid <lipengyx163@163.com>
2019-11-11 15:04:09 +0800

    基础文件提交

--- src/main/java/com/paranoid/poem/controller/WelcomeController.java
+++ src/main/java/com/paranoid/poem/controller/WelcomeController.java
@@ -1,8 +1,17 @@
 package com.paranoid.poem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Paranoid
  * @create 2019-11-11 13:57
  */
+@RestController
 public class WelcomeController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "Test";
+    }
 }
